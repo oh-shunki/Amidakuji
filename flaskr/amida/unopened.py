7d6a9127f357550bc86c182fdee2b01315dc5d45
Yuuki <inarote1204@gmail.com>
2026-04-29 17:29:33 +0900

    未開封画面　lines表示対応

--- flaskr/amida/unopened.py
+++ flaskr/amida/unopened.py
@@ -7,7 +7,9 @@ def unopened(amida):
     """⑤あみだくじ（未開封）画面制御"""
     amida_id = amida.get("amida_id")
 
+    lines=db.get_lines_from_amida(amida_id)
     results = {}
+    results["lines"] = lines
     results["option_hide_items"] = amida.get("option_hide_items")
 
     return results
