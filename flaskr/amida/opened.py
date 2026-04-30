804411e6fe9cab6a2983ec080a4d38f403926840
Yuuki <inarote1204@gmail.com>
2026-04-30 17:42:20 +0900

    opened.py 開封済み画面の基礎実装

--- flaskr/amida/opened.py
+++ flaskr/amida/opened.py
@@ -7,7 +7,25 @@ def opened(amida):
     """⑨あみだくじ（開封済）画面制御"""
     amida_id = amida.get("amida_id")
 
+    print("opened.pyに入りました")
+    print(amida)
+
+    # lines = db.get_lines_from_amida(amida_id)
+    lines = [
+        {"line_id":1,"name":"Aさん"},
+        {"line_id":2,"name":"Bさん"}
+    ]
+
     results = {}
+    results["amida_id"] = amida_id
+    results["lines"] = lines
 
     return results
+
+if __name__ == "__main__":
+    test_amida={
+        "amida_id": "test-id"
+    }
+
+    print(opened(test_amida))
 
