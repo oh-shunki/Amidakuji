46afc2dd4cfe0efc4e7f18beadb2992c11b712a7
OH Shunki <git@shunki.dev>
2026-05-11 02:01:18 +0900

    feat(unopened): 抽籤したくじにニックネーム表示の実装

--- flaskr/amida/unopened.py
+++ flaskr/amida/unopened.py
@@ -7,9 +7,19 @@ def unopened(amida):
     """⑤あみだくじ（未開封）画面制御"""
     amida_id = amida.get("amida_id")
 
-    lines=db.get_lines_from_amida(amida_id)
+    db_lines = db.get_lines_from_amida(amida_id)
     results = {}
-    results["lines"] = lines
+    results["amida_lines"] = []
     results["option_hide_items"] = amida.get("option_hide_items")
+
+    for line in db_lines:
+        status = db.LineStatus(line.get("status"))
+        if status == db.LineStatus.READY:
+            nickname = None
+        else:
+            nickname = db.get_draw_nickname_from_line(line.get("line_id"))
+
+        results["amida_lines"].append(nickname)
+
 
     return results
