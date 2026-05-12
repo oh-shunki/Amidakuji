0327ecef535b392ae2e5ec757bcd70f8169d9f36
OH Shunki <git@shunki.dev>
2026-05-12 23:29:24 +0900

    feat(create): 作成機能（処理）の実装

--- flaskr/create.py
+++ flaskr/create.py
@@ -1,8 +1,11 @@
 """あみだくじ作成制御"""
-from flask import Blueprint, redirect, render_template, request, url_for
+from werkzeug.security import generate_password_hash
+from flask import Blueprint, redirect, render_template, request, url_for, abort
 
 from . import db
-from .utils import amida_id_to_b62, amida_id_b62_to_uuid
+
+from .amida.amida_form import get_and_validate_form
+from .utils import amida_id_to_b62
 from .conform import conform
 
 bp = Blueprint("create", __name__)
@@ -11,13 +14,32 @@ bp = Blueprint("create", __name__)
 def create():
     """②あみだくじ設定画面制御"""
     if request.method == "POST":
-        pass
-        # 作成処理はここ
-        # amida_id = db.create_amida(...)
-        # if amida_id:
-        #   amida_id_b62 = amida_id_to_b62(amida_id)
-        #
-        # return redirect(url_for("create.create_conform", amida_id_b62=amida_id_b62))
+        errors, amida, amida_items = get_and_validate_form(request.form, mode="create")
+
+        if errors:
+            amida.pop("admin_password", None)
+            amida.pop("user_password", None)
+            return render_template("create.html", errors=errors,
+                                                  amida=amida,
+                                                  amida_items=amida_items)
+
+        admin_password = amida.pop("admin_password")
+        amida["admin_password_hash"] = generate_password_hash(admin_password)
+
+        user_password = amida.pop("user_password")
+        if user_password:
+            amida["user_password_hash"] = generate_password_hash(user_password)
+        else:
+            amida["user_password_hash"] = None
+
+        amida_id = db.create_amida(amida, amida_items)
+
+        if not amida_id:
+            abort(500, description="作成するとき、不明なエラーが出ました。")
+
+        amida_id_b62 = amida_id_to_b62(amida_id)
+
+        return redirect(url_for("create.create_conform", amida_id_b62=amida_id_b62))
 
     return render_template("create.html")
 
