6453b64947a39f3d55ddc7b6a1e0b2a399a49afe
OH Shunki <git@shunki.dev>
2026-05-19 22:17:17 +0900

    feat(cancel): 抽籤取消機能（処理）の実装

--- flaskr/amida/cancel.py
+++ flaskr/amida/cancel.py
@@ -1,10 +1,97 @@
-from flask import Blueprint, render_template
+"""あみだくじ抽籤取消制御"""
+from werkzeug.security import check_password_hash
+from flask import (
+    Blueprint, flash, redirect, render_template, request, url_for, abort
+    )
 
 from .user_auth import user_auth_required
 
+from .. import db
+from ..utils import amida_id_b62_to_uuid
+from ..conform import conform
+
 bp = Blueprint("cancel", __name__)
 
-@bp.route("/cancel")
+@bp.route("/", methods=("GET",))
 @user_auth_required
 def cancel(amida_id_b62):
+    """⑧取消画面制御"""
+    try:
+        amida_id = amida_id_b62_to_uuid(amida_id_b62)
+    except ValueError:
+        # このIDのはBase62型ではない
+        abort(404)
+
+    amida = db.get_amida(amida_id)
+
+    # このIDは存在していない
+    if amida is None:
+        abort(404)
+
     return render_template("amida/cancel.html")
+
+@bp.route("/do", methods=("POST",))
+@user_auth_required
+def do_cancel(amida_id_b62):
+    """取消処理制御"""
+    try:
+        amida_id = amida_id_b62_to_uuid(amida_id_b62)
+    except ValueError:
+        # このIDのはBase62型ではない
+        abort(404)
+
+    amida = db.get_amida(amida_id)
+
+    # このIDは存在していない
+    if amida is None:
+        abort(404)
+
+    # 開封済の場合はエラー
+    if amida.get("is_opened"):
+        abort(403, description="開封済みのあみだくじは抽籤取消できません。")
+
+    line_no = request.form.get("line_no", "")
+    line = db.get_line_by_no_from_amida(amida_id, line_no)
+
+    # 状態は DRAWN 以外の場合はエラー
+    line_status = db.LineStatus(line.get("status"))
+    if line_status != db.LineStatus.DRAWN:
+        abort(403, description="この抽籤は取消できません。")
+
+    # 抽籤記録がない場合はエラー
+    line_id = line.get("line_id")
+    draw = db.get_draw(line_id)
+    if draw is None:
+        abort(404)
+
+    password = request.form.get("password", "")
+    password_hash = draw.get("password_hash", "")
+
+    if password_hash is None:
+        abort(404)
+
+    error = None
+    if not password:
+        error = "パスワードが入力されていません"
+    elif not check_password_hash(password_hash, password):
+        error = "パスワードが違います"
+
+    if error:
+        flash(error)
+        return redirect(url_for("amida.cancel.cancel",
+                                amida_id_b62=amida_id_b62,
+                                line_no=line_no))
+
+    success = db.do_cancel(amida_id, line_no)
+    if success:
+        flash("成功しました")
+    else:
+        flash("失敗しました")
+
+    return redirect(url_for("amida.cancel.cancel_conform", amida_id_b62=amida_id_b62))
+
+@bp.route("/conform")
+@user_auth_required
+def cancel_conform(amida_id_b62):
+    """⑦結果確認画面（取消）制御"""
+    return conform(amida_id_b62, mode="cancel")
