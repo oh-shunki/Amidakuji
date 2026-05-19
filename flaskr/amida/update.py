cae20a16f86a31f1160e9a853ac5895c820b25d3
OH Shunki <git@shunki.dev>
2026-05-19 21:28:37 +0900

    feat(update): 設定変更機能（処理）の実装

--- flaskr/amida/update.py
+++ flaskr/amida/update.py
@@ -1,11 +1,67 @@
-from flask import Blueprint, render_template
+from werkzeug.security import generate_password_hash
+from flask import Blueprint, redirect, render_template, request, url_for, abort
 
-from . import bp
 from .user_auth import user_auth_required
+from .amida_form import get_and_validate_form
+
+from .. import db
+from ..utils import amida_id_b62_to_uuid
+from ..conform import conform
 
 bp = Blueprint("update", __name__)
 
-@bp.route("/update")
+@bp.route("/", methods=("GET", "POST"))
 @user_auth_required
 def update(amida_id_b62):
-    return render_template("amida/update.html")
+    """➉設定変更画面制御"""
+    try:
+        amida_id = amida_id_b62_to_uuid(amida_id_b62)
+    except ValueError:
+        # このIDのはBase62型ではない
+        abort(404)
+
+    amida = db.get_amida(amida_id)
+
+    # このIDは存在していない
+    if amida is None:
+        abort(404)
+
+    if request.method == "POST":
+        errors, amida, amida_items = get_and_validate_form(request.form, mode="update")
+
+        if errors:
+            amida.pop("admin_password", None)
+            amida.pop("user_password", None)
+            return render_template("update.html", errors=errors,
+                                                  amida=amida,
+                                                  amida_items=amida_items)
+
+        admin_password = amida.pop("admin_password")
+        if admin_password:
+            amida["admin_password_hash"] = generate_password_hash(admin_password)
+        else:
+            amida["admin_password_hash"] = None
+
+        user_password = amida.pop("user_password")
+        if user_password:
+            amida["user_password_hash"] = generate_password_hash(user_password)
+        else:
+            amida["user_password_hash"] = None
+
+        amida["amida_id"] = amida_id_b62_to_uuid(amida_id_b62)
+
+        success = db.update_amida(amida, amida_items)
+
+        if not success:
+            abort(500, description="設定変更するとき、不明なエラーが出ました。")
+
+        return redirect(url_for("update.update_conform", amida_id_b62=amida_id_b62))
+
+    amida_items = db.get_items_from_amida(amida_id)
+
+    return render_template("amida/update.html", amida=amida, amida_items=amida_items)
+
+@bp.route("/conform")
+def update_conform(amida_id_b62):
+    """⑪変更確認画面制御"""
+    return conform(amida_id_b62, mode="update")
