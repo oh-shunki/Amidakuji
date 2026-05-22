b393853a682fcd165cc0e6303b8652e22fbdf1df
OH Shunki <git@shunki.dev>
2026-05-22 17:07:57 +0900

    fix(unopened): unopened.py に option_hide_items により、アイテムのランダム並び替えの修正
    - option_hide_items をテンプレートに渡す
    - option_hide_items オンの時、アイテムをランダムにならびかえる

--- flaskr/amida/unopened.py
+++ flaskr/amida/unopened.py
@@ -1,4 +1,5 @@
 """あみだくじ（未開封）制御"""
+import random
 from flask import flash, redirect, request, url_for, abort
 
 from .. import db
@@ -10,8 +11,14 @@ def unopened(amida):
     results = {}
     amida_nicknames = db.get_nicknames_from_amida(amida_id) or []
     amida_items = db.get_items_from_amida(amida_id) or []
+    option_hide_items = amida.get("option_hide_items")
+
+    # ランダムに渡す
+    if option_hide_items:
+        random.shuffle(amida_items)
 
     results["amida_nicknames"] = amida_nicknames
     results["amida_items"] = amida_items
+    results["option_hide_items"] = option_hide_items
 
     return results
