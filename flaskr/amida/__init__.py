e5b50c7d85b9009a9a9df98fe7b41b5596ce6edb
OH Shunki <git@shunki.dev>
2026-05-23 22:15:48 +0900

    feat(amida): __init__.py の update の bp をアンコメントした

--- flaskr/amida/__init__.py
+++ flaskr/amida/__init__.py
@@ -11,8 +11,8 @@ bp.register_blueprint(user_auth.bp, url_prefix="/user_auth")
 from . import nickname
 bp.register_blueprint(nickname.bp, url_prefix="/nickname")
 
-#from . import update
-#bp.register_blueprint(update.bp, url_prefix="/update")
+from . import update
+bp.register_blueprint(update.bp, url_prefix="/update")
 
 #from . import cancel
 #bp.register_blueprint(cancel.bp, url_prefix="/cancel")
