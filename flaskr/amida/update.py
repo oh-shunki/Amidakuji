353b08b28a4942e210904532dda9300ec0bafc4d
OH Shunki <git@shunki.dev>
2026-05-24 02:46:42 +0900

    refactor(update): 画面制御と設定変更実行を分離した
    - /<amida_id_b62>/update    GET  画面制御
    - /<amida_id_b62>/update/do POST 設定変更実行

--- flaskr/amida/update.py
+++ flaskr/amida/update.py
@@ -14,7 +14,7 @@ from ..conform import conform
 
 bp = Blueprint("update", __name__)
 
-@bp.route("/", methods=("GET", "POST"))
+@bp.route("/", methods=("GET",))
 @user_auth_required
 def update(amida_id_b62):
     """➉設定変更画面制御"""
@@ -44,45 +44,6 @@ def update(amida_id_b62):
                                                     amida_items=amida_items)
     """
 
-    if request.method == "POST":
-        errors, amida, amida_items = get_and_validate_form(request.form, mode="update")
-
-        if errors:
-            amida.pop("admin_password", None)
-            amida.pop("user_password", None)
-            return render_template("update.html", errors=errors,
-                                                  amida=amida,
-                                                  amida_items=amida_items)
-
-        admin_password = amida.pop("admin_password")
-        if admin_password:
-            amida["admin_password_hash"] = generate_password_hash(admin_password)
-        else:
-            amida["admin_password_hash"] = None
-
-        user_password = amida.pop("user_password")
-        if user_password:
-            amida["user_password_hash"] = generate_password_hash(user_password)
-        else:
-            amida["user_password_hash"] = None
-
-        amida["amida_id"] = amida_id_b62_to_uuid(amida_id_b62)
-
-        # アイテムを足して、ランダムに並び替える
-        line_count = db.get_line_count_from_amida(amida_id)
-        shortage = line_count - len(amida_items)
-        if shortage > 0:
-            amida_items.extend([""] * shortage)
-
-        random.shuffle(amida_items)
-
-        success = db.update_amida(amida, amida_items)
-
-        if not success:
-            abort(500, description="設定変更するとき、不明なエラーが出ました。")
-
-        return redirect(url_for("amida.update.update_conform", amida_id_b62=amida_id_b62))
-
     # テンプレートに渡さない項目を削除
     amida.pop("admin_password", None)
     amida.pop("user_password", None)
@@ -93,7 +54,59 @@ def update(amida_id_b62):
     amida_items = [item for item in amida_items if item["title"]]
     print(amida_items)
 
-    return render_template("amida/update.html", amida=amida, amida_items=amida_items)
+    return render_template("amida/update.html", amida_id_b62=amida_id_b62,
+                                                amida=amida,
+                                                amida_items=amida_items)
+
+@bp.route("/do", methods=("POST",))
+@user_auth_required
+def do_update(amida_id_b62):
+    """設定変更実行"""
+    try:
+        amida_id = amida_id_b62_to_uuid(amida_id_b62)
+    except ValueError:
+        # このIDのはBase62型ではない
+        abort(404)
+
+    amida = db.get_amida(amida_id)
+
+    # このIDは存在していない
+    if amida is None:
+        abort(404)
+
+    errors, amida, amida_items = get_and_validate_form(request.form, mode="update")
+
+    if errors:
+        abort(500, description="更新するとき、不明なエラーが出ました。")
+
+    admin_password = amida.pop("admin_password")
+    if admin_password:
+        amida["admin_password_hash"] = generate_password_hash(admin_password)
+    else:
+        amida["admin_password_hash"] = None
+
+    user_password = amida.pop("user_password")
+    if user_password:
+        amida["user_password_hash"] = generate_password_hash(user_password)
+    else:
+        amida["user_password_hash"] = None
+
+    amida["amida_id"] = amida_id_b62_to_uuid(amida_id_b62)
+
+    # アイテムを足して、ランダムに並び替える
+    line_count = db.get_line_count_from_amida(amida_id)
+    shortage = line_count - len(amida_items)
+    if shortage > 0:
+        amida_items.extend([""] * shortage)
+
+    random.shuffle(amida_items)
+
+    success = db.update_amida(amida, amida_items)
+
+    if not success:
+        abort(500, description="設定変更するとき、不明なエラーが出ました。")
+
+    return redirect(url_for("amida.update.update_conform", amida_id_b62=amida_id_b62))
 
 @bp.route("/conform")
 def update_conform(amida_id_b62):
